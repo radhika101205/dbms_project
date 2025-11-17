976494221bf0983fce720f535ebb9c22c69d5817
Radhika Agarwal <radhikaa908@gmail.com>
2025-11-17 21:59:58 +0530

    Saving my local changes before committing

--- toydb/pflayer/hf.h
+++ toydb/pflayer/hf.h
@@ -24,6 +24,13 @@ typedef struct {
 } HF_SlotEntry;
 
 
+/* A Record ID (RID) uniquely identifies a record in the file.
+ * It consists of the page number and the slot number.
+ */
+typedef struct {
+    int pageNum;
+    int slotNum;
+} RID;
 /*
  * Define some error codes for the HF layer
  */
@@ -40,15 +47,113 @@ typedef struct {
 void HF_InitPage(char *pageBuf);
 
 // Inserts a new record
-int HF_InsertRec(char *pageBuf, char *record, int recLen);
+int HF_Page_InsertRec(char *pageBuf, char *record, int recLen);
 
 // Deletes a record
-int HF_DeleteRec(char *pageBuf, int slotNum);
+int HF_Page_DeleteRec(char *pageBuf, int slotNum);
 
 // Gets a specific record
-int HF_GetRec(char *pageBuf, int slotNum, char **record, int *recLen);
+int HF_Page_GetRec(char *pageBuf, int slotNum, char **record, int *recLen);
 
 // Gets the next valid record
-int HF_GetNextRec(char *pageBuf, int currentSlotNum, char **record, int *recLen);
+int HF_Page_GetNextRec(char *pageBuf, int currentSlotNum, char **record, int *recLen);
+
+/*
+ * Creates a new, empty heap file.
+ */
+int HF_CreateFile(char *fileName);
+
+/*
+ * Opens an existing heap file.
+ * Returns a file descriptor (fd) from the PF layer.
+ */
+int HF_OpenFile(char *fileName);
+
+/*
+ * Closes a heap file.
+ */
+int HF_CloseFile(int fd);
+
+/*
+ * Inserts a record into the file.
+ *
+ * Inputs:
+ * fd: The file descriptor
+ * record: Pointer to the record data
+ * recLen: Length of the record
+ *
+ * Outputs:
+ * rid: The RID of the newly inserted record
+ *
+ * Returns:
+ * HFE_OK on success, or an error code
+ */
+int HF_InsertRec(int fd, char *record, int recLen, RID *rid);
+
+/*
+ * Deletes a record from the file, given its RID.
+ */
+int HF_DeleteRec(int fd, RID rid);
+
+/*
+ * Retrieves a record from the file, given its RID.
+ *
+ * Outputs:
+ * record: Pointer to the record data *within the buffer page*
+ * recLen: Length of the record
+ */
+int HF_GetRec(int fd, RID rid, char **record, int *recLen);
+
+/*
+ * ======================================================
+ * File Scan Function Prototypes
+ * ======================================================
+ */
+
+// This struct will keep track of the scanner's state
+typedef struct {
+    int   fd;             // The file descriptor
+    int   currentPageNum; // Page number of the current page
+    int   currentSlotNum; // Slot number of the last record found
+    char *currentPageBuf; // Pinned buffer for the current page
+} HF_Scan;
+
+
+/*
+ * Opens a new file scan.
+ *
+ * Inputs:
+ * fd: The file descriptor
+ *
+ * Outputs:
+ * scan: A pointer to an HF_Scan struct that will be
+ * initialized by this function.
+ *
+ * Returns:
+ * HFE_OK on success
+ */
+int HF_OpenFileScan(int fd, HF_Scan *scan);
+
+/*
+ * Retrieves the next valid record in the file scan.
+ *
+ * Inputs:
+ * scan: The HF_Scan struct initialized by OpenFileScan
+ *
+ * Outputs:
+ * rid: The RID of the next record
+ * record: Pointer to the record's data
+ * recLen: Length of the record
+ *
+ * Returns:
+ * HFE_OK on success
+ * HFE_EOF when no more records are found
+ */
+int HF_GetNextRec(int fd, HF_Scan *scan, RID *rid, char **record, int *recLen);
+
+/*
+ * Closes a file scan.
+ */
+int HF_CloseFileScan(HF_Scan *scan);
 
 #endif // HF_H
